8dacc42d091620221819c920f115d4f2674d55ac
BAEK DONGWOO <qorehddn123@naver.com>
2021-01-14 17:02:02 +0900

    redux toolkit 완료

--- README.md
+++ README.md
@@ -18,3 +18,16 @@
 ### connect()
 ![CommentStructure](./studyPic_redux/connect.png) <br/>
 - props에 추가될 수 있도록 허용(위 사진을 적용시킬시 props에 컴포넌트가 한개 추가)
+
+# yarn add @reduxjs/toolkit
+![CommentStructure](./studyPic_redux/toolkit.png) <br/>
+- toolkit을 사용한 createAction을 사용시 이렇게 찍힌다(payload에 찍히는걸 확인 가능) 
+### createReducer
+- 사용시 switch,case 문을 사용 안해도됨
+![CommentStructure](./studyPic_redux/createRuducer.png) <br/>
+- state를 변경시킴(push는 mutate를 해준다, filter는 안해줌(그래서 return을 사용해야함))
+### configureStore
+- const store = configureStore({ reducer }); (사용시 Redux Developer Tools 사용가능)(Redux toolkit를 사용하지않아도 configureStore 사용시 가능)
+### createSlice
+- reducer 와 action 생성을 해준다. 
+![CommentStructure](./studyPic_redux/create.png) <br/>
--- src/store.js
+++ src/store.js
@@ -1,31 +1,89 @@
 import { createStore } from 'redux';
+//
+// const ADD = 'ADD';
+// const DELETE = 'DELETE';
+//
+// export const addToDo = (text) => {
+//     return {
+//         type: ADD,
+//         text,
+//     };
+// };
+// export const deleteTodo = (id) => {
+//     return {
+//         type: DELETE,
+//         id: parseInt(id),
+//     };
+// };
+// const reducer = (state = [], action) => {
+//     switch (action.type) {
+//         case ADD:
+//             return [{ text: action.text, id: Date.now() }, ...state];
+//         case DELETE:
+//             return state.filter((toDo) => toDo.id !== action.id);
+//         default:
+//             return state;
+//     }
+// };
+// const store = createStore(reducer);
+//
+// export const actionCreators = { addToDo, deleteTodo };
+// export default store;
+//
+//
+//
+//
+//        ----------이 아래 문장은 REDUX toolkit ------------
+import {
+    configureStore,
+    createAction,
+    createReducer,
+    createSlice,
+} from '@reduxjs/toolkit';
 
 const ADD = 'ADD';
 const DELETE = 'DELETE';
 
-export const addToDo = (text) => {
-    return {
-        type: ADD,
-        text,
-    };
-};
-export const deleteTodo = (id) => {
-    return {
-        type: DELETE,
-        id: parseInt(id),
-    };
-};
-const reducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD:
-            return [{ text: action.text, id: Date.now() }, ...state];
-        case DELETE:
-            return state.filter((toDo) => toDo.id !== action.id);
-        default:
-            return state;
-    }
-};
-const store = createStore(reducer);
+// const addToDo = createAction('ADD');
+// const deleteTodo = createAction('DELETE');
 
-export const actionCreators = { addToDo, deleteTodo };
+// const reducer = (state = [], action) => {
+//     switch (action.type) {
+//         case addToDo.type:
+//             return [{ text: action.payload, id: Date.now() }, ...state];
+//         case deleteTodo.type:
+//             return state.filter((toDo) => toDo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// };
+// const addToDo = createAction('ADD');
+// const deleteTodo = createAction('DELETE');
+//
+// const reducer = createReducer([], {
+//     [addToDo]: (state, action) => {
+//         state.push({ text: action.payload, id: Date.now() });
+//     },
+//     [deleteTodo]: (state, action) => {
+//         return state.filter((toDo) => toDo.id !== action.payload);
+//     },
+// }); // 초기값
+const toDos = createSlice({
+    name: 'toDosReducer',
+    initialState: [],
+    reducers: {
+        add: (state, action) => {
+            state.push({ text: action.payload, id: Date.now() });
+        },
+        remove: (state, action) => {
+            return state.filter((toDo) => toDo.id !== action.payload);
+        },
+    },
+});
+const store = configureStore({ reducer: toDos.reducer });
+// const store = createStore(reducer);
+
+// export const actionCreators = { addToDo, deleteTodo };
+
+export const { add, remove } = toDos.actions;
 export default store;
